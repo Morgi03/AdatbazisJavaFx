423539455c58f8bfdf3973fe91b97e8dc85008fd
Morgován Nimród <morgovannimrod22@gmail.com>
2022-12-08 09:34:23 +0100

    dog-view elkezdése (buttonok még nincsenek)

--- src/main/java/hu/petrik/adatbazisjavafx/DogController.java
+++ src/main/java/hu/petrik/adatbazisjavafx/DogController.java
@@ -2,13 +2,20 @@ package hu.petrik.adatbazisjavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class DogController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TableView<Dog> dogTable;
+    @FXML
+    private TableColumn<Dog, String> nameCol;
+    @FXML
+    private TableColumn<Dog, Integer> ageCol;
+    @FXML
+    private TableColumn<Dog, String> breedCol;
+
+
+
 }
